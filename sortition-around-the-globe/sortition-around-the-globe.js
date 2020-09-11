88a08441886f560793929a495cf4f5f809b9407c
dg-western <david.western@uwe.ac.uk>
2020-09-11 16:15:48 +0100

    Fix all-black markers bug.

--- sortition-around-the-globe/sortition-around-the-globe.js
+++ sortition-around-the-globe/sortition-around-the-globe.js
@@ -57,7 +57,7 @@ function initMap() {
         var markers2 = [];
 
         function rgbToHex(r, g, b) {
-          return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+          return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
         }
 
         function yearToColour(year) {
